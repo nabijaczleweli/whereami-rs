7c0009db24a1a550f519e90d05fbb57316c327bd
nabijaczleweli <nabijaczleweli@gmail.com>
2016-07-20 10:40:34 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! [`whereami`](https://github.com/gpakosz/whereami) for Rust.
+//!
+//! Used for checking where the current executable/DLL is.
+//!
+//! # Examples:
+//!
+//! ```
+//! println!("This executable is at {}", whereami::executable_path().unwrap().to_str().unwrap());
+//! println!("This exec/DLL is at {}", whereami::module_path().unwrap().to_str().unwrap());
+//! ```
+
 extern crate libc;
 
 mod native;
@@ -6,10 +17,29 @@ mod implementation;
 use std::path::PathBuf;
 
 
+/// Returns the path of the current executable or `None` if acquiring thereof failed.
+///
+/// # Examples:
+///
+/// ```
+/// # use whereami::executable_path;
+/// println!("This executable is at {}", executable_path().unwrap().to_str().unwrap());
+/// ```
 pub fn executable_path() -> Option<PathBuf> {
     implementation::get_path(native::wai_getExecutablePath)
 }
 
+/// Returns the path of the current module or `None` if acquiring thereof failed.
+///
+/// Note: this is not the Rust `mod`, but rather an OS module
+///       (most of the time: DLL)
+///
+/// # Examples:
+///
+/// ```
+/// # use whereami::module_path;
+/// println!("This exec/DLL is at {}", module_path().unwrap().to_str().unwrap());
+/// ```
 pub fn module_path() -> Option<PathBuf> {
     implementation::get_path(native::wai_getModulePath)
 }
